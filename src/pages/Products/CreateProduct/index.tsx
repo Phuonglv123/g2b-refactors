16436f9cc8e34397438b8b3ce157435fa1c86252
Kai <smledgehulk@proton.me>
2024-10-29 22:15:49 +0700

    f

--- src/pages/Products/CreateProduct/index.tsx
+++ src/pages/Products/CreateProduct/index.tsx
@@ -65,7 +65,7 @@ const CreateProduct: React.FC = () => {
     const payloadLocation: ILocation = {
       international: values.international ? true : false,
       country: values.international ? values?.country : 'Việt nam',
-      city: values.city,
+      city: values.city.replace('Thành phố', '').replace('Tỉnh', '').replace('- ', '-'),
       ward: values.ward,
       district: values.district,
       address: values.address,
